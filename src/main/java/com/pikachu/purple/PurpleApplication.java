1d34e481b56093a28e18fa3fec8cb731d581764f
wonjunYou <youwjune@gmail.com>
2024-03-19 21:29:12 +0900

    init project

---
+++ src/main/java/com/pikachu/purple/PurpleApplication.java
@@ -0,0 +1,13 @@
+package com.pikachu.purple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pikachu/purple/PurpleApplicationTests.java
@@ -0,0 +1,11 @@
+//package com.pikachu.purple;
+//
+//import org.junit.jupiter.api.Test;
+//
+//class PurpleApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
