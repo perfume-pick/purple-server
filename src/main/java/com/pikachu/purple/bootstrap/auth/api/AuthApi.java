422b9c7ad35587b3b471dd5d7fd76bf3abe5e0bf
wonjunYou <youwjune@gmail.com>
2024-04-20 03:40:35 +0900

    [PURPLE-52] import 누락 해결

--- src/main/java/com/pikachu/purple/bootstrap/auth/api/AuthApi.java
+++ src/main/java/com/pikachu/purple/bootstrap/auth/api/AuthApi.java
@@ -1,16 +1,16 @@
 package com.pikachu.purple.bootstrap.auth.api;
 
+import com.pikachu.purple.bootstrap.auth.dto.request.EmailVerificationRequest;
 import com.pikachu.purple.bootstrap.auth.dto.response.SocialLoginTryResponse;
 import com.pikachu.purple.domain.user.enums.SocialLoginProvider;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import com.pikachu.purple.bootstrap.auth.dto.request.EmailVerificationRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -25,7 +25,7 @@ public interface AuthApi {
         @PathVariable("provider") SocialLoginProvider provider
 
     );
-  
+
     @Operation(summary = "회원가입시 이메일 인증코드 전송")
     @ApiResponses(
         value = {
@@ -40,7 +40,7 @@ public interface AuthApi {
             )
         }
     )
-  
+
     @PostMapping("/verify-code/send")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void send(
--- src/main/java/com/pikachu/purple/bootstrap/auth/controller/AuthController.java
+++ src/main/java/com/pikachu/purple/bootstrap/auth/controller/AuthController.java
@@ -1,15 +1,15 @@
 package com.pikachu.purple.bootstrap.auth.controller;
 
+import com.pikachu.purple.application.user.port.in.SendEmailVerificationUseCase;
 import com.pikachu.purple.application.user.port.in.SocialLoginTryUseCase;
 import com.pikachu.purple.application.user.port.in.SocialLoginTryUseCase.Command;
 import com.pikachu.purple.application.user.port.in.SocialLoginTryUseCase.Result;
 import com.pikachu.purple.bootstrap.auth.api.AuthApi;
+import com.pikachu.purple.bootstrap.auth.dto.request.EmailVerificationRequest;
 import com.pikachu.purple.bootstrap.auth.dto.response.SocialLoginTryResponse;
 import com.pikachu.purple.domain.user.enums.SocialLoginProvider;
-import lombok.RequiredArgsConstructor;
-import com.pikachu.purple.application.user.port.in.SendEmailVerificationUseCase;
-import com.pikachu.purple.bootstrap.auth.dto.request.EmailVerificationRequest;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,4 +33,5 @@ public class AuthController implements AuthApi {
     public void send(@RequestBody @Valid EmailVerificationRequest request){
         sendEmailVerificationUseCase.invoke(request.getEmail());
     }
+
 }
