bb5e6285083e8b71aad8e893e27a6f0ceb644912
wonjunYou <youwjune@gmail.com>
2024-04-21 00:13:31 +0900

    [PURPLE-000] CORS 해제

--- src/main/java/com/pikachu/purple/bootstrap/common/config/WebMvcConfig.java
+++ src/main/java/com/pikachu/purple/bootstrap/common/config/WebMvcConfig.java
@@ -13,15 +13,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Value("${uri.client}")
     private String clientUri;
 
-    @Value("${uri.server}")
-    private String serverUri;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/perpicks")
             .allowedHeaders("*")
-            .allowedOrigins(clientUri, serverUri, "http://localhost:3000")
+            .allowedOrigins(clientUri, "http://localhost:3000")
             .allowedMethods("*");
     }
 
