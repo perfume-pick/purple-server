c6530f0a618091584f6160af1c0a75eea9b52d3b
wonjunYou <youwjune@gmail.com>
2024-04-22 10:32:14 +0900

    [PURPLE-000] CORS 프론트 IP로 제한적 허용

--- src/main/java/com/pikachu/purple/bootstrap/common/config/WebMvcConfig.java
+++ src/main/java/com/pikachu/purple/bootstrap/common/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.pikachu.purple.bootstrap.common.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,12 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Value("${uri.client}")
+    private String devClientUri;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/**")
             .allowedHeaders("*")
-            .allowedOrigins("*")
+            .allowedOrigins(devClientUri, "http://localhost:3000")
             .allowedMethods("*");
     }
 
