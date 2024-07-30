5887dbd94e04ec4c5663d2843975653e6bd92da9
choejonghyeon <choijonghyen@gmail.com>
2024-07-30 21:57:18 +0900

    [PURPLE-107] redis key 상수로 변환

--- src/main/java/com/pikachu/purple/infrastructure/redis/user/repository/impl/UserSearchHistoryRedisRepositoryImpl.java
+++ src/main/java/com/pikachu/purple/infrastructure/redis/user/repository/impl/UserSearchHistoryRedisRepositoryImpl.java
@@ -13,13 +13,14 @@ public class UserSearchHistoryRedisRepositoryImpl implements
     UserSearchHistoryRedisRepository {
 
     private static final int MAX_SIZE = 9;
+    private static final String KEY = "searchHistory:";
 
     private final RedisTemplate<String, Object> redisTemplate;
 
     @Override
     public List<UserSearchHistoryRedisHash> findSearchHistoryByUserId(Long userId){
         return (List<UserSearchHistoryRedisHash>) (List<?>) redisTemplate.opsForList().range(
-            "searchHistory:" + userId,
+            KEY + userId,
             0,
             MAX_SIZE
         );
@@ -30,13 +31,13 @@ public class UserSearchHistoryRedisRepositoryImpl implements
         Long userId,
         UserSearchHistoryRedisHash userSearchHistoryRedisHash
     ) {
-        Long getCount = redisTemplate.opsForList().size("searchHistory:" + userId);
+        Long getCount = redisTemplate.opsForList().size(KEY + userId);
         redisTemplate.opsForList().leftPush(
-            "searchHistory:" + userId,
+            KEY + userId,
             userSearchHistoryRedisHash
         );
         if(getCount == 10) redisTemplate.opsForList().trim(
-            "searchHistory:" + userId,
+            KEY + userId,
             0,
             MAX_SIZE
         );
@@ -45,7 +46,7 @@ public class UserSearchHistoryRedisRepositoryImpl implements
 
     @Override
     public void deleteAllSearchHistory(Long userId) {
-        redisTemplate.delete("searchHistory:" + userId);
+        redisTemplate.delete(KEY + userId);
     }
 
 }
