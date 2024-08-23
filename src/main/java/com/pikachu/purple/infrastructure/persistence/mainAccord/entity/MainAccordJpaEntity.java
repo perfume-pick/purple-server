226420439df5e3e5550a24d2d12c1e538f6c8e18
dj.jeong <sam1087@mju.ac.kr>
2024-08-24 01:45:48 +0900

    [PURPLE-124] MainAccord Entity 및 Domain 추가

---
+++ src/main/java/com/pikachu/purple/infrastructure/persistence/mainAccord/entity/MainAccordJpaEntity.java
@@ -0,0 +1,28 @@
+package com.pikachu.purple.infrastructure.persistence.mainAccord.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "main_accord")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MainAccordJpaEntity {
+    @Id
+    @Column(name = "main_accord_id")
+    private Long mainAccordId;
+
+    @Column(name = "perfume_id", nullable = false)
+    private Long perfumeId;
+
+    @Column(name = "note_name", nullable = false)
+    private String noteName;
+
+    @Column(name = "accord_value", nullable = false)
+    private Integer accordValue;
+}
