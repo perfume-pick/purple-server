83baaf6f8149a2f87801abc92f840ffd63d033c1
choejonghyeon <choijonghyen@gmail.com>
2024-08-27 21:51:51 +0900

    [PURPLE-125] 피드백 4차 반영

--- src/main/java/com/pikachu/purple/bootstrap/perfume/api/PerfumeApi.java
+++ src/main/java/com/pikachu/purple/bootstrap/perfume/api/PerfumeApi.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@Tag(name = "perfume", description = "perfume API")
+@Tag(name = "Perfume", description = "Perfume API")
 @RequestMapping(value = "/perpicks/perfumes", produces = "application/json")
 public interface PerfumeApi {
 
