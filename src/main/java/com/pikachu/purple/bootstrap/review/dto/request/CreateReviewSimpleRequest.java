4573054ae3c93fb9c61b97999948fad3ddbf0949
choejonghyeon <choijonghyen@gmail.com>
2024-09-04 13:10:48 +0900

    [PURPLE-126] 컨벤션에 맞는 클래스명 리팩토링

--- src/main/java/com/pikachu/purple/bootstrap/review/dto/request/CreateReviewSimpleRequest.java
+++ src/main/java/com/pikachu/purple/bootstrap/review/dto/request/CreateReviewSimpleRequest.java
@@ -3,8 +3,8 @@ package com.pikachu.purple.bootstrap.review.dto.request;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.Range;
 
-public record ReviewUpdateRequest (
-    Long ratingId,
+public record CreateReviewSimpleRequest(
+    Long perfumeId,
     @Range(min = 1, max = 5)
     int score,
     @Size(min = 10, max = 300, message = "최소 10자 ~ 최대 300자 입력할 수 있습니다.")
