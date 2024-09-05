41e6751b0499f63674a164d9b014340cab5a5567
dj.jeong <sam1087@mju.ac.kr>
2024-09-05 17:26:51 +0900

    [PURPLE-132] @UtilityClass 어노테이션 추가

--- src/main/java/com/pikachu/purple/application/util/MathUtil.java
+++ src/main/java/com/pikachu/purple/application/util/MathUtil.java
@@ -1,5 +1,8 @@
 package com.pikachu.purple.application.util;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class MathUtil {
 
     public static int getPercentage(int part, int whole) {
