7b8e168ae7d26168e0e7d47f3acc0f385728188c
dj.jeong <sam1087@mju.ac.kr>
2024-09-06 16:43:33 +0900

    [PURPLE-137] Git 파일명 대수문자 구분 처리

---
+++ src/main/java/com/pikachu/purple/application/perfume/common/dto/PerfumeAccordDTO.java
@@ -0,0 +1,19 @@
+package com.pikachu.purple.application.perfume.common.dto;
+
+import com.pikachu.purple.domain.perfume.PerfumeAccord;
+
+public record PerfumeAccordDTO(
+    int order,
+    String noteName,
+    int accordValue
+) {
+
+    public static PerfumeAccordDTO of(int order, PerfumeAccord perfumeAccord) {
+        return new PerfumeAccordDTO(
+            order,
+            perfumeAccord.getNoteName(),
+            perfumeAccord.getAccordValue()
+        );
+    }
+
+}
