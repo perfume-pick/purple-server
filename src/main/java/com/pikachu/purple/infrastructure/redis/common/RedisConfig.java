028e89f70534ede0ccaa6c2277977a6cf8b10e2a
Dongjun Jeong <sam1087@mju.ac.kr>
2024-09-13 16:41:17 +0900

    [PURPLE-145] 분산락을 위한 어노테이션 구현 (#90)
    * [PURPLE-145] 분산락을 위한 어노테이션 구현
    
    * [PURPLE-145] Redisson 패키지 추가
    
    * [PURPLE-145] 컨벤션에 따른 띄어쓰기 수정
    
    * [PURPLE-145] 컨벤션에 따른 띄어쓰기 수정2
    
    * [PURPLE-145] 컨벤션에 따른 띄어쓰기 수정3
    
    * [PURPLE-145] tryLock 시 IllegalMonitorStateException 에러 처리 추가
    
    * [PURPLE-145] tryLock 시 IllegalMonitorStateException 에러 처리 수정
    
    * [PURPLE-145] tryLock 시 IllegalMonitorStateException 에러 처리 수정 2
    
    * [PURPLE-145] sonarcloud 이슈 처리
    - Synchronize on this "Lock" object using "acquire/release".
    
    * [PURPLE-145] 불필요한 어노테이션 제거
    
    * [PURPLE-145] 불필요한 import 제거

--- src/main/java/com/pikachu/purple/infrastructure/redis/common/RedisConfig.java
+++ src/main/java/com/pikachu/purple/infrastructure/redis/common/RedisConfig.java
@@ -1,10 +1,14 @@
 package com.pikachu.purple.infrastructure.redis.common;
 
+import lombok.RequiredArgsConstructor;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -12,6 +16,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfig {
     @Value(value = "${spring.data.redis.host}")
@@ -20,13 +25,29 @@ public class RedisConfig {
     @Value(value = "${spring.data.redis.port}")
     private int port;
 
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
     @Bean
-    public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(host, port);
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress(
+            REDISSON_HOST_PREFIX
+                + host
+                + ":"
+                + port
+        );
+
+        return Redisson.create(config);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(){
+    public RedisConnectionFactory redisConnectionFactory(){
+        return new RedissonConnectionFactory(redissonClient());
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
