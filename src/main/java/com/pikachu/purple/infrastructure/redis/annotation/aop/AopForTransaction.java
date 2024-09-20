5374eef2b3fdab228fe4c9dae962fcdc17b98f62
dj.jeong <sam1087@mju.ac.kr>
2024-09-20 20:25:41 +0900

    [PURPLE-147] NESTED 사용불가로 인한 롤백

--- src/main/java/com/pikachu/purple/infrastructure/redis/annotation/aop/AopForTransaction.java
+++ src/main/java/com/pikachu/purple/infrastructure/redis/annotation/aop/AopForTransaction.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 public class AopForTransaction {
 
-    @Transactional(propagation = Propagation.NESTED)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Object proceed(final ProceedingJoinPoint joinPoint) throws Throwable {
         return joinPoint.proceed();
     }
