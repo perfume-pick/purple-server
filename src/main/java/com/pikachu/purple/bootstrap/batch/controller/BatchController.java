7aab8384c782aa01e6ca98ffdd184667a0f060fe
dj.jeong <sam1087@mju.ac.kr>
2024-10-09 16:46:13 +0900

    [PURPLE-000] 수동 배치 API 구현

---
+++ src/main/java/com/pikachu/purple/bootstrap/batch/controller/BatchController.java
@@ -0,0 +1,27 @@
+package com.pikachu.purple.bootstrap.batch.controller;
+
+import com.pikachu.purple.application.statistic.scheduler.EvaluationStatisticScheduler;
+import com.pikachu.purple.application.statistic.scheduler.StarRatingStatisticScheduler;
+import com.pikachu.purple.bootstrap.batch.api.BatchApi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BatchController implements BatchApi {
+
+    private final StarRatingStatisticScheduler starRatingStatisticScheduler;
+    private final EvaluationStatisticScheduler evaluationStatisticScheduler;
+
+    // TODO: Batch로 리팩토링 필요
+    @Override
+    public void countStarRatingStatistics() {
+        starRatingStatisticScheduler.dailyRecountStarRatingStatistics();
+    }
+
+    // TODO: Batch로 리팩토링 필요
+    @Override
+    public void countEvaluationStatistics() {
+        evaluationStatisticScheduler.dailyRecountEvaluationStatistics();
+    }
+}
