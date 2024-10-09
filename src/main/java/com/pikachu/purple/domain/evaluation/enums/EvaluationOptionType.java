9d8706f37af8342f78b845ddf0309c88986591a4
dj.jeong <sam1087@mju.ac.kr>
2024-10-09 12:43:03 +0900

    [PURPLE-000] 평가 옵션명(=설명) 수정

--- src/main/java/com/pikachu/purple/domain/evaluation/enums/EvaluationOptionType.java
+++ src/main/java/com/pikachu/purple/domain/evaluation/enums/EvaluationOptionType.java
@@ -11,16 +11,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum EvaluationOptionType {
 
-    LONGEVITY_VERY_WEEK("EO101", "매우 약함"),
-    LONGEVITY_WEEK("EO102", "약함"),
-    LONGEVITY_MODERATE("EO103", "보통"),
-    LONGEVITY_LONG_LASTING("EO104", "오래감"),
-    LONGEVITY_ETERNAL("EO105", "매우 오래감"),
+    LONGEVITY_VERY_WEEK("EO101", "매우 약해요"),
+    LONGEVITY_WEEK("EO102", "약해요"),
+    LONGEVITY_MODERATE("EO103", "보통이에요"),
+    LONGEVITY_LONG_LASTING("EO104", "오래가요"),
+    LONGEVITY_ETERNAL("EO105", "매우 오래가요"),
 
-    SILLAGE_INTIMATE("EO201", "향 여운이 약함"),
-    SILLAGE_MODERATE("EO202", "보통"),
-    SILLAGE_STRONG("EO203", "향 여운이 강함"),
-    SILLAGE_ENORMOUS("EO204", "향 여운이 매우 강함"),
+    SILLAGE_INTIMATE("EO201", "향 여운이 약해요"),
+    SILLAGE_MODERATE("EO202", "보통이에요"),
+    SILLAGE_STRONG("EO203", "향 여운이 강해요"),
+    SILLAGE_ENORMOUS("EO204", "향 여운이 매우 강해요"),
 
     SEASON_TIME_SPRING("EO301", "봄"),
     SEASON_TIME_SUMMER("EO302", "여름"),
@@ -29,11 +29,11 @@ public enum EvaluationOptionType {
     SEASON_TIME_DAY("EO305", "낮"),
     SEASON_TIME_NIGHT("EO306", "밤"),
 
-    GENDER_MALE("EO401", "남성"),
-    GENDER_MORE_MALE("EO402", "남성에 가까운"),
-    GENDER_UNISEX("EO403", "중성"),
-    GENDER_MORE_FEMALE("EO404", "여성에 가까운"),
-    GENDER_FEMALE("EO405", "여성");
+    GENDER_MALE("EO401", "남성적이에요"),
+    GENDER_MORE_MALE("EO402", "남성에 가까워요"),
+    GENDER_UNISEX("EO403", "중성적이에요"),
+    GENDER_MORE_FEMALE("EO404", "여성에 가까워요"),
+    GENDER_FEMALE("EO405", "여성적이에요");
 
     private final String code;
     private final String name;
