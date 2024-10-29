d5c5ae4552ce556af11efa9441dd7308492fae13
dj.jeong <sam1087@mju.ac.kr>
2024-10-29 21:17:14 +0900

    [PURPLE-172] RecommendedPerfumeDTO 관련 평점 산출 로직 적용

--- src/main/java/com/pikachu/purple/application/perfume/service/application/perfume/GetPerfumesByReviewCountApplicationService.java
+++ src/main/java/com/pikachu/purple/application/perfume/service/application/perfume/GetPerfumesByReviewCountApplicationService.java
@@ -3,6 +3,7 @@ package com.pikachu.purple.application.perfume.service.application.perfume;
 import com.pikachu.purple.application.perfume.common.dto.RecommendedPerfumeDTO;
 import com.pikachu.purple.application.perfume.port.in.perfume.GetPerfumesByReviewCountUseCase;
 import com.pikachu.purple.application.perfume.service.domain.PerfumeDomainService;
+import com.pikachu.purple.application.review.port.in.starrating.GetAverageScoreByPerfumeIdUseCase;
 import com.pikachu.purple.domain.perfume.Perfume;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ public class GetPerfumesByReviewCountApplicationService implements
     GetPerfumesByReviewCountUseCase {
 
     private final PerfumeDomainService perfumeDomainService;
+    private final GetAverageScoreByPerfumeIdUseCase getAverageScoreByPerfumeIdUseCase;
 
     private static final int MAX_SIZE = 30;
 
@@ -22,6 +24,11 @@ public class GetPerfumesByReviewCountApplicationService implements
     @Override
     public Result invoke() {
        List<Perfume> perfumes =  perfumeDomainService.findAllOrderByReviewCount(MAX_SIZE);
+        for (Perfume perfume : perfumes) {
+            double averageScore = getAverageScoreByPerfumeIdUseCase.invoke(
+                new GetAverageScoreByPerfumeIdUseCase.Command(perfume.getId())).averageScore();
+            perfume.setAverageScore(averageScore);
+        }
 
        List<RecommendedPerfumeDTO> recommendedPerfumeDTOs = perfumes.stream()
            .map(perfume -> RecommendedPerfumeDTO.from(
