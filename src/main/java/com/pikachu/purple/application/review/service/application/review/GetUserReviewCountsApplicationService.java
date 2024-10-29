2794b93bae9200309ae181354870bf8426d323b0
Dongjun Jeong <sam1087@mju.ac.kr>
2024-10-30 01:32:47 +0900

    [PURPLE-182] 사용자가 평가한 향수 개수 조회 API 응답 정보 형식 변경 (#126)

--- src/main/java/com/pikachu/purple/application/review/service/application/review/GetUserReviewCountsApplicationService.java
+++ src/main/java/com/pikachu/purple/application/review/service/application/review/GetUserReviewCountsApplicationService.java
@@ -2,7 +2,6 @@ package com.pikachu.purple.application.review.service.application.review;
 
 import static com.pikachu.purple.support.security.SecurityProvider.getCurrentUserAuthentication;
 
-import com.pikachu.purple.application.review.common.dto.UserReviewCountsDTO;
 import com.pikachu.purple.application.review.port.in.review.GetUserReviewCountsUseCase;
 import com.pikachu.purple.application.review.service.domain.ReviewDomainService;
 import com.pikachu.purple.application.user.port.in.user.GetUserCountsUseCase;
@@ -24,12 +23,10 @@ public class GetUserReviewCountsApplicationService implements GetUserReviewCount
         int userCounts = getUserCountsUseCase.invoke().userCounts();
         double averageUserReviewCounts = Math.round((double) totalReviewCounts / userCounts);
 
-        UserReviewCountsDTO userReviewCountsDTO = UserReviewCountsDTO.builder()
-            .currentUserReviewCounts(currentUserReviewCounts)
-            .averageUserReviewCounts((int) averageUserReviewCounts)
-            .build();
-
-        return new Result(userReviewCountsDTO);
+        return new Result(
+            currentUserReviewCounts,
+            (int) averageUserReviewCounts
+        );
     }
 
 }
--- src/main/java/com/pikachu/purple/bootstrap/user/dto/response/GetUserReviewCountsResponse.java
+++ src/main/java/com/pikachu/purple/bootstrap/user/dto/response/GetUserReviewCountsResponse.java
@@ -1,8 +1,8 @@
 package com.pikachu.purple.bootstrap.user.dto.response;
 
-import com.pikachu.purple.application.review.common.dto.UserReviewCountsDTO;
-
 public record GetUserReviewCountsResponse(
-    UserReviewCountsDTO userReviewCountsDTO) {
+    int currentUserReviewCounts,
+    int averageUserReviewCounts
+) {
 
 }
