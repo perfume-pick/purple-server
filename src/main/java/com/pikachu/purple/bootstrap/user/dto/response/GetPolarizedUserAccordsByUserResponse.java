6b5c1155d745790b12d95c4f6bdcdfe13f37996b
choejonghyeon <choijonghyen@gmail.com>
2024-10-29 21:15:41 +0900

    [PURPLE-175] 코드 컨벤션 적용

--- src/main/java/com/pikachu/purple/bootstrap/user/dto/response/GetPolarizedUserAccordsByUserResponse.java
+++ src/main/java/com/pikachu/purple/bootstrap/user/dto/response/GetPolarizedUserAccordsByUserResponse.java
@@ -2,6 +2,4 @@ package com.pikachu.purple.bootstrap.user.dto.response;
 
 import com.pikachu.purple.application.user.common.dto.PolarizedUserAccordDTO;
 
-public record GetPolarizedUserAccordsByUserResponse(PolarizedUserAccordDTO userAccord) {
-
-}
+public record GetPolarizedUserAccordsByUserResponse(PolarizedUserAccordDTO userAccord) {}
