25dc4fe3d505c7ea34f2c076dc3fe96e00f2f7dd
choejonghyeon <choijonghyen@gmail.com>
2024-11-10 19:17:06 +0900

    [PURPLE-000] 피드백 반영

--- src/main/java/com/pikachu/purple/bootstrap/auth/api/AuthApi.java
+++ src/main/java/com/pikachu/purple/bootstrap/auth/api/AuthApi.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URISyntaxException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
