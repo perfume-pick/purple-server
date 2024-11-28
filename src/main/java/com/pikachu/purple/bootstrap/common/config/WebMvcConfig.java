003c85894fa71ea4f03c2f2436c39eb4e14f9223
dj.jeong <sam1087@mju.ac.kr>
2024-11-28 22:25:56 +0900

    [PURPLE-000] cors 에러 조치 2

--- src/main/java/com/pikachu/purple/bootstrap/common/config/WebMvcConfig.java
+++ src/main/java/com/pikachu/purple/bootstrap/common/config/WebMvcConfig.java
@@ -21,8 +21,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/**")
+            .allowedOriginPatterns("*")
             .allowedHeaders("*")
-            .allowedOrigins("*")
             .allowedMethods("*")
             .allowCredentials(true);
     }
