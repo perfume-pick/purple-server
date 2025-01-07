6e9ebc9cf2b3a5fc5d80a47f025f801b6e51d52e
Dongjun Jeong <sam1087@mju.ac.kr>
2025-01-07 21:14:56 +0900

    [PURPLE-000] 향수 평점 갱신 로직 임시 주석 처리 (#177)
    분산락이 적용된 별점 집계 갱신 처리 후 콜백으로 처리하도록 추후 수정

--- src/main/java/com/pikachu/purple/application/review/service/application/starrating/CreateStarRatingApplicationService.java
+++ src/main/java/com/pikachu/purple/application/review/service/application/starrating/CreateStarRatingApplicationService.java
@@ -37,8 +37,9 @@ public class CreateStarRatingApplicationService implements CreateStarRatingUseCa
             )
         );
 
-        recalculatePerfumeAverageScoreApplicationService.invoke(
-            new RecalculatePerfumeAverageScoreApplicationService.Command(command.perfumeId()));
+        // TODO: 별점 갱신 후 콜백으로 처리
+//        recalculatePerfumeAverageScoreApplicationService.invoke(
+//            new RecalculatePerfumeAverageScoreApplicationService.Command(command.perfumeId()));
 
         return new Result(starRating);
     }
