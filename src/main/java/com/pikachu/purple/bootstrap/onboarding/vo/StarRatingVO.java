ea9b5b2b6ee9dbbea68b1b01823c4d65f0fb551a
choejonghyeon <choijonghyen@gmail.com>
2025-01-16 15:15:03 +0900

    [PURPLE-221] OnBoarding Api 유효성 검증 추가

--- src/main/java/com/pikachu/purple/bootstrap/onboarding/vo/StarRatingVO.java
+++ src/main/java/com/pikachu/purple/bootstrap/onboarding/vo/StarRatingVO.java
@@ -1,8 +1,12 @@
 package com.pikachu.purple.bootstrap.onboarding.vo;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.hibernate.validator.constraints.Range;
 
 public record StarRatingVO(
+    @NotNull
+    @Positive
     Long perfumeId,
 
     @Range(min = 1, max = 5)
