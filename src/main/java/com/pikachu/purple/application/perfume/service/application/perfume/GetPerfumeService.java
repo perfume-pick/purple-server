ad9aa9d4d259d762713b77ba03e037b18a9d8505
choejonghyeon <choijonghyen@gmail.com>
2025-01-23 01:18:55 +0900

    [PERPICK-009] GetPerfumeService 명명 규칙 반영

--- src/main/java/com/pikachu/purple/application/perfume/service/application/perfume/GetPerfumeService.java
+++ src/main/java/com/pikachu/purple/application/perfume/service/application/perfume/GetPerfumeService.java
@@ -1,22 +1,18 @@
 package com.pikachu.purple.application.perfume.service.application.perfume;
 
-import com.pikachu.purple.application.perfume.port.in.perfume.GetPerfumeByIdUseCase;
+import com.pikachu.purple.application.perfume.port.in.perfume.GetPerfumeUseCase;
 import com.pikachu.purple.application.perfume.service.domain.PerfumeDomainService;
 import com.pikachu.purple.domain.perfume.Perfume;
-import com.pikachu.purple.infrastructure.persistence.perfume.adaptor.PerfumeAccordJpaAdaptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-class GetPerfumeByIdApplicationService implements GetPerfumeByIdUseCase {
+class GetPerfumeService implements GetPerfumeUseCase {
 
     private final PerfumeDomainService perfumeDomainService;
-    private final PerfumeAccordJpaAdaptor perfumeAccordJpaAdaptor;
-
     @Override
     public Result invoke(Long perfumeId) {
-        perfumeAccordJpaAdaptor.findAllByPerfumeIdOrderByValueDesc(null, 0);
         Perfume perfume = perfumeDomainService.findById(perfumeId);
 
         return new Result(perfume);
