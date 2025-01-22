aff947e4a067804d74db93fec897a0f19d35f410
dj.jeong <sam1087@mju.ac.kr>
2025-01-23 01:56:33 +0900

    [PERPICK-013] (review > complaint) Create/DeleteComplaintUseCase 리팩토링
    ## 리팩토링 사항
    - In Port UseCase 클래스 명명 규칙 적용 + 유스케이스 분리/통합(오버로딩)
    - Service 클래스 명명 규칙 적용
    - 유즈케이스 행위는 같지만 파라미터가 다를 경우 → 오버로딩
    -  메소드명 invoke()로 통일
    - Command 제거
    
    ### 수정 유스케이스 목록
    - CreateComplaintUseCase
    - DeleteComplaintUseCase
    
    ### 자세한 리팩토링 기준 참고
    > https://ember-bluebell-522.notion.site/c23039c416554ae19a4e4aa11ce77d0c?pvs=4

--- src/main/java/com/pikachu/purple/application/review/service/application/complaint/CreateComplaintService.java
+++ src/main/java/com/pikachu/purple/application/review/service/application/complaint/CreateComplaintService.java
@@ -2,28 +2,31 @@ package com.pikachu.purple.application.review.service.application.complaint;
 
 import static com.pikachu.purple.support.security.SecurityProvider.getCurrentUserAuthentication;
 
-import com.pikachu.purple.application.review.port.in.complaint.DeleteComplaintUseCase;
+import com.pikachu.purple.application.review.port.in.SendComplaintUseCase;
+import com.pikachu.purple.application.review.port.in.complaint.CreateComplaintUseCase;
 import com.pikachu.purple.application.review.service.domain.ComplaintDomainService;
 import com.pikachu.purple.domain.review.Complaint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-class DeleteComplaintApplicationService implements DeleteComplaintUseCase {
+class CreateComplaintService implements CreateComplaintUseCase {
 
     private final ComplaintDomainService complaintDomainService;
+    private final SendComplaintUseCase sendComplaintUseCase;
 
+    @Transactional
     @Override
     public void invoke(Long reviewId) {
         Long userId = getCurrentUserAuthentication().userId();
-
-        Complaint complaint = complaintDomainService.find(
+        Complaint complaint = complaintDomainService.create(
             userId,
             reviewId
         );
 
-        complaintDomainService.delete(complaint.getId());
+        sendComplaintUseCase.invoke(complaint);
     }
 
 }
