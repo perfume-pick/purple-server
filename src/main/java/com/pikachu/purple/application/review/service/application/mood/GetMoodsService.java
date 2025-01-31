1e9891cc7441539b609b6b8e14f369c5891f0b54
Dongjun Jeong <sam1087@mju.ac.kr>
2025-01-31 10:22:46 +0900

    [PERPICK-017] (review > mood) GetMoodsUseCase 리팩토링 (#200)
    ## 리팩토링 사항
    - In Port UseCase 클래스 명명 규칙 적용 + 유스케이스 분리/통합(오버로딩)
    - Service 클래스 명명 규칙 적용
    - 유즈케이스 행위는 같지만 파라미터가 다를 경우 → 오버로딩
    -  메소드명 invoke()로 통일
    - Command 제거
    
    ### 자세한 리팩토링 기준 참고
    > https://ember-bluebell-522.notion.site/c23039c416554ae19a4e4aa11ce77d0c?pvs=4

--- src/main/java/com/pikachu/purple/application/review/service/application/mood/GetMoodsService.java
+++ src/main/java/com/pikachu/purple/application/review/service/application/mood/GetMoodsService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-class GetMoodsApplicationService implements GetMoodsUseCase {
+class GetMoodsService implements GetMoodsUseCase {
 
     private final MoodDomainService moodDomainService;
 
