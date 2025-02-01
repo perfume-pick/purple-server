1f038098a55173364e4e096ceeeed9ea82dd59ba
choejonghyeon <choijonghyen@gmail.com>
2025-02-01 11:26:13 +0900

    [PERPICK-031] RecalculatePerfumeAverageScoreService 리팩토링

--- src/main/java/com/pikachu/purple/application/perfume/service/application/perfume/RecalculatePerfumeAverageScoreService.java
+++ src/main/java/com/pikachu/purple/application/perfume/service/application/perfume/RecalculatePerfumeAverageScoreService.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-class RecalculatePerfumeAverageScoreApplicationService implements
+class RecalculatePerfumeAverageScoreService implements
     RecalculatePerfumeAverageScoreUseCase {
 
     private final PerfumeDomainService perfumeDomainService;
