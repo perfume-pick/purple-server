8117d12ae6e1db8fe5cf2c12e206c92ba1fb6c04
choejonghyeon <choijonghyen@gmail.com>
2025-02-02 11:31:26 +0900

    [PERPICK-028] 리뷰 반영

--- src/main/java/com/pikachu/purple/bootstrap/perfume/controller/BrandController.java
+++ src/main/java/com/pikachu/purple/bootstrap/perfume/controller/BrandController.java
@@ -2,7 +2,7 @@ package com.pikachu.purple.bootstrap.perfume.controller;
 
 import com.pikachu.purple.application.perfume.port.in.brand.GetBrandsUseCase;
 import com.pikachu.purple.application.perfume.port.in.brand.GetBrandsUseCase.Result;
-import com.pikachu.purple.application.perfume.port.in.perfume.GetPerfumesWithBrandUseCase;
+import com.pikachu.purple.application.perfume.port.in.brand.GetBrandsUseCase.ResultBrandPerfumesDTO;
 import com.pikachu.purple.bootstrap.common.dto.SuccessResponse;
 import com.pikachu.purple.bootstrap.perfume.api.BrandApi;
 import com.pikachu.purple.bootstrap.perfume.dto.response.GetBrandsResponse;
@@ -16,18 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class BrandController implements BrandApi {
 
     private final GetBrandsUseCase getBrandsUseCase;
-    private final GetPerfumesWithBrandUseCase getPerfumesWithBrandUseCase;
 
     @Override
     public SuccessResponse<GetBrandsResponse> findAll(){
-        Result result = getBrandsUseCase.invoke();
+        Result result = getBrandsUseCase.findAll();
 
         return SuccessResponse.of(new GetBrandsResponse(result.brandDTOs()));
     }
 
     @Override
-    public SuccessResponse<GetPerfumesByBrandNamesResponse> findAllPerfumesByBrandNames(List<String> brandNames) {
-        GetPerfumesWithBrandUseCase.Result result = getPerfumesWithBrandUseCase.invoke(brandNames);
+    public SuccessResponse<GetPerfumesByBrandNamesResponse> findAllWithPerfumes(List<String> brandNames) {
+        ResultBrandPerfumesDTO result = getBrandsUseCase.findAllWithPerfumes(brandNames);
 
         return SuccessResponse.of(new GetPerfumesByBrandNamesResponse(result.brandPerfumesDTOs()));
     }
--- src/main/java/com/pikachu/purple/infrastructure/persistence/perfume/repository/BrandJpaRepository.java
+++ src/main/java/com/pikachu/purple/infrastructure/persistence/perfume/repository/BrandJpaRepository.java
@@ -1,8 +1,17 @@
 package com.pikachu.purple.infrastructure.persistence.perfume.repository;
 
 import com.pikachu.purple.infrastructure.persistence.perfume.entity.BrandJpaEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BrandJpaRepository extends JpaRepository<BrandJpaEntity, String> {}
+public interface BrandJpaRepository extends JpaRepository<BrandJpaEntity, String> {
+
+    @Query("select b from BrandJpaEntity b "
+        + "left join fetch b.perfumes "
+        + "where b.name in :names")
+    List<BrandJpaEntity> findByNameIn(List<String> names);
+
+}
