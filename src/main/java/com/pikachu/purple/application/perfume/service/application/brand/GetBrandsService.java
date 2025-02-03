37264f56744e19ef6dae5423768d9b4653aea2f2
choejonghyeon <choijonghyen@gmail.com>
2025-02-03 16:56:26 +0900

    [PERPICK-028] 리뷰 2차 반영

--- src/main/java/com/pikachu/purple/application/perfume/service/application/brand/GetBrandsService.java
+++ src/main/java/com/pikachu/purple/application/perfume/service/application/brand/GetBrandsService.java
@@ -9,9 +9,11 @@ import com.pikachu.purple.domain.perfume.Brand;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 class GetBrandsService implements GetBrandsUseCase {
 
     private final BrandDomainService brandDomainService;
--- src/main/java/com/pikachu/purple/infrastructure/persistence/perfume/repository/BrandJpaRepository.java
+++ src/main/java/com/pikachu/purple/infrastructure/persistence/perfume/repository/BrandJpaRepository.java
@@ -3,15 +3,12 @@ package com.pikachu.purple.infrastructure.persistence.perfume.repository;
 import com.pikachu.purple.infrastructure.persistence.perfume.entity.BrandJpaEntity;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BrandJpaRepository extends JpaRepository<BrandJpaEntity, String> {
 
-    @Query("select b from BrandJpaEntity b "
-        + "left join fetch b.perfumes "
-        + "where b.name in :names")
+    //TODO N+1 문제 발생
     List<BrandJpaEntity> findByNameIn(List<String> names);
 
 }
