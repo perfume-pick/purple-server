ffb93c67d10dd10151ee5239a60f3e7c42b8f65a
Dongjun Jeong <sam1087@mju.ac.kr>
2025-02-25 01:35:49 +0900

    [PERPICK-62] (review) GetEvaluationFormFieldUseCase 관련 반환값(Result), 응답정보(Response) 리팩토링 (#246)

--- src/main/java/com/pikachu/purple/bootstrap/review/dto/response/GetEvaluationFormFieldResponse.java
+++ src/main/java/com/pikachu/purple/bootstrap/review/dto/response/GetEvaluationFormFieldResponse.java
@@ -1,11 +1,31 @@
 package com.pikachu.purple.bootstrap.review.dto.response;
 
+import com.pikachu.purple.application.review.port.in.GetEvaluationFormFieldUseCase.Result;
 import com.pikachu.purple.domain.evaluation.EvaluationField;
 import com.pikachu.purple.domain.evaluation.EvaluationOption;
 import com.pikachu.purple.domain.review.Mood;
 import java.util.List;
+import lombok.Getter;
 
-public record GetEvaluationFormFieldResponse(
-    List<EvaluationField<EvaluationOption>> evaluationFields,
-    List<Mood> moods
-) {}
+@Getter
+public class GetEvaluationFormFieldResponse {
+
+    private final List<EvaluationField<EvaluationOption>> evaluationFields;
+    private final List<Mood> moods;
+
+    private GetEvaluationFormFieldResponse(
+        List<EvaluationField<EvaluationOption>> evaluationFields,
+        List<Mood> moods
+    ) {
+        this.evaluationFields = evaluationFields;
+        this.moods = moods;
+    }
+
+    public static GetEvaluationFormFieldResponse of(Result result) {
+        return new GetEvaluationFormFieldResponse(
+            result.evaluationFields(),
+            result.moods()
+        );
+    }
+
+}
