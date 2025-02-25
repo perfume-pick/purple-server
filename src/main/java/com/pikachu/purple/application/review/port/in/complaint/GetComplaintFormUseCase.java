7316511128ffa4e91f1ba6222db963a51e103be4
dj.jeong <sam1087@mju.ac.kr>
2025-02-25 11:12:11 +0900

    [PERPICK-065] UseCase DTO 매핑 방식 변경

--- src/main/java/com/pikachu/purple/application/review/port/in/complaint/GetComplaintFormUseCase.java
+++ src/main/java/com/pikachu/purple/application/review/port/in/complaint/GetComplaintFormUseCase.java
@@ -1,7 +1,5 @@
 package com.pikachu.purple.application.review.port.in.complaint;
 
-import com.pikachu.purple.domain.review.Complaint;
-import com.pikachu.purple.domain.review.Review;
 import java.time.Instant;
 
 public interface GetComplaintFormUseCase {
@@ -21,24 +19,6 @@ public interface GetComplaintFormUseCase {
         String perfumeName,
         String comment,
         String link
-    ) {
-
-        public static ComplaintFormDTO from(
-            Review reportedReview,
-            Complaint complaint,
-            String reviewUri
-        ) {
-            return new ComplaintFormDTO(
-                complaint.getReportedAt(),
-                complaint.getId(),
-                reportedReview.getUser().getId(),
-                reportedReview.getPerfume().getId(),
-                reportedReview.getPerfume().getName(),
-                reportedReview.getContent(),
-                reviewUri + "/" + complaint.getId() + "?token=" + complaint.getToken()
-            );
-        }
-
-    }
+    ) {}
 
 }
