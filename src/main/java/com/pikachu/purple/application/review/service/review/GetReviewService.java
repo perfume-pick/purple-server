9f608fbe0f3ca8eeb38ee32cdd9dededdb444f1e
dj.jeong <sam1087@mju.ac.kr>
2025-02-27 19:04:36 +0900

    [PERPICK-066] 불필요한 import 제거

--- src/main/java/com/pikachu/purple/application/review/service/review/GetReviewService.java
+++ src/main/java/com/pikachu/purple/application/review/service/review/GetReviewService.java
@@ -1,16 +1,11 @@
 package com.pikachu.purple.application.review.service.review;
 
-import com.pikachu.purple.application.review.common.dto.ReviewByUserDTO;
-import com.pikachu.purple.application.review.common.dto.ReviewEvaluationFieldDTO;
-import com.pikachu.purple.application.review.common.dto.ReviewEvaluationOptionDTO;
 import com.pikachu.purple.application.review.port.in.review.GetReviewUseCase;
 import com.pikachu.purple.application.review.port.out.ReviewRepository;
 import com.pikachu.purple.application.review.port.out.StarRatingRepository;
-import com.pikachu.purple.domain.review.Mood;
 import com.pikachu.purple.domain.review.Review;
 import com.pikachu.purple.domain.review.StarRating;
 import com.pikachu.purple.domain.review.enums.ReviewType;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
