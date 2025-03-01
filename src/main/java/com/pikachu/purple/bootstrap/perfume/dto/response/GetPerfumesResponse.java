d52b0654ccc4fa4a52a958c3d007df5c36f9b125
JongHyeon Choi <61353820+JHyeonC@users.noreply.github.com>
2025-03-02 00:10:19 +0900

    [PERPICK-073] (perfume > perfume) GetPerfumesUseCase 관련 반환값(Result), 응답정보(Response) 리팩토링 (#256)

--- src/main/java/com/pikachu/purple/bootstrap/perfume/dto/response/GetPerfumesResponse.java
+++ src/main/java/com/pikachu/purple/bootstrap/perfume/dto/response/GetPerfumesResponse.java
@@ -1,6 +1,53 @@
 package com.pikachu.purple.bootstrap.perfume.dto.response;
 
-import com.pikachu.purple.application.perfume.common.dto.PerfumeDTO;
-import java.util.List;
+import static com.pikachu.purple.bootstrap.common.exception.BusinessException.PerfumeAccordNotFoundException;
 
-public record GetPerfumesResponse(List<PerfumeDTO> perfumes) {}
+import com.pikachu.purple.application.perfume.port.in.perfume.GetPerfumesUseCase;
+import com.pikachu.purple.application.util.IdUtil;
+import com.pikachu.purple.domain.accord.Accord;
+import com.pikachu.purple.domain.perfume.Perfume;
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class GetPerfumesResponse {
+    private final List<PerfumeDTO> perfumes;
+
+    public GetPerfumesResponse(List<PerfumeDTO> perfumes) {
+        this.perfumes = perfumes;
+    }
+
+    public static GetPerfumesResponse of(GetPerfumesUseCase.Result result) {
+        List<PerfumeDTO> perfumes = result.perfumes().stream()
+            .map(PerfumeDTO::from)
+            .toList();
+
+        return new GetPerfumesResponse(perfumes);
+    }
+
+    record PerfumeDTO(
+        String perfumeId,
+        String perfumeName,
+        String brandName,
+        String imageUrl,
+        double averageScore,
+        String accordName
+    ) {
+
+        public static PerfumeDTO from(
+            Perfume perfume) {
+            return new PerfumeDTO(
+                IdUtil.toString(perfume.getId()),
+                perfume.getKoreanName(),
+                perfume.getBrand().getKoreanName(),
+                perfume.getImageUrl(),
+                perfume.getAverageScore(),
+                perfume.getAccords().stream()
+                    .map(Accord::getKoreanName)
+                    .findFirst()
+                    .orElseThrow(() -> PerfumeAccordNotFoundException)
+            );
+        }
+    }
+
+}
--- src/main/java/com/pikachu/purple/bootstrap/recommend/dto/response/GetPerfumesByReviewCountsResponse.java
+++ src/main/java/com/pikachu/purple/bootstrap/recommend/dto/response/GetPerfumesByReviewCountsResponse.java
@@ -1,8 +1,53 @@
 package com.pikachu.purple.bootstrap.recommend.dto.response;
 
-import com.pikachu.purple.application.perfume.common.dto.RecommendedPerfumeDTO;
+import com.pikachu.purple.application.perfume.port.in.perfume.GetPerfumesUseCase;
+import com.pikachu.purple.application.util.IdUtil;
+import com.pikachu.purple.domain.perfume.Perfume;
+import com.pikachu.purple.domain.perfume.PerfumeAccord;
 import java.util.List;
 
-public record GetPerfumesByReviewCountsResponse(List<RecommendedPerfumeDTO> perfumes) {
+
+public class GetPerfumesByReviewCountsResponse {
+    private final List<RecommendedPerfumeDTO> perfumes;
+
+    public GetPerfumesByReviewCountsResponse(List<RecommendedPerfumeDTO> perfumes) {
+        this.perfumes = perfumes;
+    }
+
+    public static GetPerfumesByReviewCountsResponse of(GetPerfumesUseCase.Result result) {
+        List<RecommendedPerfumeDTO> perfumes = result.perfumes().stream()
+            .map(perfume -> RecommendedPerfumeDTO.from(
+                perfume,
+                perfume.getAccords() == null ? List.of() : perfume.getAccords().stream()
+                    .map(PerfumeAccord::getKoreanName)
+                    .toList()
+            ))
+            .toList();
+        return new GetPerfumesByReviewCountsResponse(perfumes);
+    }
+
+    record RecommendedPerfumeDTO(
+        String perfumeId,
+        String name,
+        String brandName,
+        String imageUrl,
+        double averageScore,
+        List<String> accordNames
+    ) {
+
+        public static RecommendedPerfumeDTO from(
+            Perfume perfume,
+            List<String> accordNames
+        ) {
+            return new RecommendedPerfumeDTO(
+                IdUtil.toString(perfume.getId()),
+                perfume.getKoreanName(),
+                perfume.getBrand().getKoreanName(),
+                perfume.getImageUrl(),
+                perfume.getAverageScore(),
+                accordNames
+            );
+        }
+    }
 
 }
