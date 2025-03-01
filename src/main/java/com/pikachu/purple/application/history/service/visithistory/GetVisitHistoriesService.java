3cfe2e6f48a598f7f92bb9982ef304c9901518de
JongHyeon Choi <61353820+JHyeonC@users.noreply.github.com>
2025-03-01 14:18:59 +0900

    [PERPICK-072] (history) Service 클래스명 수정 (#255)

--- src/main/java/com/pikachu/purple/application/history/service/visithistory/GetVisitHistoriesService.java
+++ src/main/java/com/pikachu/purple/application/history/service/visithistory/GetVisitHistoriesService.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-class GetVisitHistoriesApplicationService implements GetVisitHistoriesUseCase {
+class GetVisitHistoriesService implements GetVisitHistoriesUseCase {
 
     private final VisitHistoryRepository visitHistoryRepository;
 
